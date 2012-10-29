5840e59db1bfed8d01603c6949d18af3f503552f
Sebastian Morr <sebastian@morr.cc>
2012-10-29 15:47:34 +0100

    Don't open new tabs all the time. Hint for less F5's.

--- README.markdown
+++ README.markdown
@@ -3,7 +3,9 @@ Description
 
 Simple demo application for the optimizer. Takes an image as first parameter, then displays a HSV-variation in firefox, asks for a score, and tries to optimize. Repeat.
 
-To build, issue `make`.
+To build, issue `make`. The optimizer has to live parellel to this directory.
+
+If you don't want to press <F5> all the time, install a plugin like *Auto Reload*.
 
 Sample invocation
 -----------------
--- src/optimize-hsv.cpp
+++ src/optimize-hsv.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main(int argc, char const *argv[]) {
     string source_img(argv[1]);
-    string target_img = source_img+"-optimized.png";
+    string target_img = source_img+"~";
     string image_viewer = "firefox ";
     string convert_cmd = "convert ";
 
@@ -19,16 +19,18 @@ int main(int argc, char const *argv[]) {
     o.insert(Vec3f(50,50,200));
     o.insert(Vec3f(20,20,20));
 
+    system((image_viewer+target_img).c_str());
+
     while (!o.done()) {
         ostringstream v0, v1, v2;
         v0 << v[0];
         v1 << v[1];
         v2 << v[2];
         string cmd;
-        cmd = (convert_cmd+source_img).append(" -modulate ").append(v0.str()).append(",").append(v1.str()).append(",").append(v2.str()).append(" ").append(target_img);
+        cmd = (convert_cmd+source_img).append(" -modulate ").append(v0.str()).append(",").append(v1.str()).append(",").append(v2.str()).append(" ").append(target_img).append("~");
         cout << cmd << endl;
         system(cmd.c_str());
-        system((image_viewer+target_img).c_str());
+        system(string("cp ").append(target_img).append("~ ").append(target_img).c_str());
 
         cout << "Score? ";
         cin >> score;
